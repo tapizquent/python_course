3e1e1b129c04499d933b7344a4bc22c7611e739f
Jose Tapizquent <josetapizq@gmail.com>
2020-07-02 18:25:14 -0400

    Added initial excercise to be completed before next class

---
+++ Exercise1/number_operations.py
@@ -0,0 +1,39 @@
+# Directions:
+# - Add the missing part so the programs compiles and runs
+# - When the program runs, before anything else, prompt the user with the following
+#   message: 'What is your name? '
+# And then write to the screen: 'Hello, FIRST_NAME LAST_NAME. Welcome to addition!'
+#
+# - The user should be able to input 2 numbers (this is fine if it's done in two lines)
+# - Then print to the screen: the sum, multiplication, subtraction and exponent (a^b) of these 2 numbers
+#   that is, if user inputs 1 and 2,
+#   the output should be
+#   => SUM: 3
+#      SUB: -1
+#      MULT: 2
+#      EXP: 1
+
+# Ask user for their first and last name here
+
+first_name = input('Input your first name: ')
+last_name = ....
+
+print()
+print('Hello, ' + ......... + 'Welcome to addition!')
+print()
+
+# Ask user to input first number
+a = int(input(........))
+# Ask user to input second number
+b = int(input(........))
+
+sum = .......
+subt = .....
+mult = .....
+exp = .....
+
+print()
+print('SUM: ' + .....)
+print('SUB: ' + .....)
+print('MULT: ' + .....)
+print('EXP: ' + .....)
