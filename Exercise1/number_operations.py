282d94885f656eeee755206b9ad059f0357887e7
Dariella <dariella.rodriguez.herrera@gmail.com>
2020-08-11 00:31:09 -0400

    calculator excersice division not complete

--- Exercise1/number_operations.py
+++ Exercise1/number_operations.py
@@ -16,27 +16,85 @@
 #      EXP: 1
 
 # Ask user for their first and last name here
-print('what is your name? ')
+# print('what is your name? ')
 
-first_name = input('Input your first name: ' )
-last_name = input('Imput you last name: ')
+# first_name = input('Input your first name: ' )
+# last_name = input('Imput you last name: ')
 
-print()
-print('Hello, ' + first_name + ' '+ last_name + ' Welcome to addition!')
-print()
+# print()
+# print('Hello, ' + first_name + ' '+ last_name + ' Welcome to addition!')
+# print()
 
-# Ask user to input first number
-a = int(input('Input first number: '))
-# Ask user to input second number
-b = int(input('Input second number: '))
+# # Ask user to input first number
+# a = int(input('Input first number: '))
+# # Ask user to input second number
+# b = int(input('Input second number: '))
 
-sum = a + b
-subt = a - b
-mult = a * b
-exp = a ** b
+# sum = a + b
+# subt = a - b
+# mult = a * b
+# exp = a ** b
 
-print()
-print('SUM: ' + str(sum))
-print('SUB: ' + str(subt))
-print('MULT: ' + str(mult))
-print('EXP: ' + str(exp))
+# print()
+# print(f'SUM: {sum}')
+# print('SUB: ' + str(subt))
+# print('MULT: ' + str(mult))
+# print('EXP: ' + str(exp))
+
+# print()
+
+# print('New hiring process ')
+
+# # Ask user for profession
+# # Ask user for years of experience
+# print('What is your profession? ')
+# print('How many years of experience ? ')
+# profession = input( 'What is your profession: ')
+# years_exp= int(input('Years of experience: '))
+# print(profession +' with '+ str(years_exp) + ' years of experience ' )
+
+
+#Build a calculator in the console. Ask user for the name, print hello and the name.
+#ask the user what operation they want to do. If they type 0=add, 1=sub, 2=mult, 3=div
+#4=exp, 5= all  
+#ask the user to imput two numbers and the numbers can be floats
+#print
+
+print('What is your name? ')
+name = input('Name: ')
+print('Hello ' + name )
+print('What operation do you want to do? ')
+print('0=add, 1= sub, 2= mult, 3= div, 4= exp, 5= all')
+operation = int(input('operation: '))
+a = float(input ('Number a: '))
+b = float(input('Number b: '))
+if operation == 5: 
+    print (a+b) 
+    print (a-b)
+    print (a*b)
+    print (a**b)
+    if b!=0:
+        print (float(a//b))
+    else:
+        print('Error ')
+
+elif operation == 0:
+    print (a+b)
+elif operation == 1:
+    print (a-b)
+elif operation == 2:
+    print (a*b)
+elif operation == 4:
+    print (a**b)
+elif operation == 3:
+    if b!=0:
+        print(float(a//b))
+    else:
+        print('Error')
+else:
+    print('Good Bye ')
+
+
+
+
+
---
+++ Exercise1/suma.py
@@ -0,0 +1,25 @@
+# input a and b values as integers
+# add a plus b and print
+
+# a = int(input("a = "))
+# b = int(input("b = "))
+# print("a + b = " + str(a+b))
+
+# #remember to cast when concatenating a string to anything that is not a string
+
+# numeros = [a,b]
+# print(numeros)
+
+# pedir al usuario, nombre, edad, estatura, guardar en diccionario e imprimir el diccionario
+
+nombre = input("Nombre =")
+edad = int(input("Edad ="))
+estatura = float(input("Estatura ="))
+
+datos = {
+    "nombre": nombre,
+    "edad": edad,
+    "estatura": estatura
+}
+
+print(datos)
